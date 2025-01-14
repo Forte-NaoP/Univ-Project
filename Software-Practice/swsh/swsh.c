5693c701e9265404c53fd69dfd10c6d9f1a30136
Forte-NaoP <bae9750@gmail.com>
2025-01-14 12:09:31 +0900

    Implement command redirection and enhance PATH setup in main shell loop

--- Software-Practice/swsh/swsh.c
+++ Software-Practice/swsh/swsh.c
@@ -13,24 +13,52 @@
 
 #include "mystring.h"
 #include "argparse.h"
+#include "commands/inline.h"
 
-void chld_handler(int signo) {
-    pid_t pid;
-    int status;
-    while((pid = waitpid(-1, &status, WUNTRACED)) > 0) {
-        if(WIFSTOPPED(status)) {
-            kill(-pid, SIGKILL);
-        }
+#define INPUT_PIPE "./swsh_input"
+#define OUTPUT_PIPE "./swsh_output"
+
+const char* custom_binary = "./commands/bin:";
+
+void setup_redirection(COMMAND *cmd, int is_first, int is_last, int prev_pipe_fd, int pipe_fds[2]) {
+    if (!is_first) {
+        dup2(prev_pipe_fd, STDIN_FILENO);
+        close(prev_pipe_fd);
+    } else if (cmd->input) {
+        int fd = open(cmd->input, O_RDONLY);
+        dup2(fd, STDIN_FILENO);
+        close(fd);
     }
+
+    if (!is_last) {
+        dup2(pipe_fds[1], STDOUT_FILENO);
+        close(pipe_fds[1]);
+    } else if (cmd->output) {
+        int fd = open(cmd->output, O_WRONLY | O_CREAT | (cmd->append ? O_APPEND : O_TRUNC), 0666);
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+    }
+}
+
+void restore_redirection(int stdin_fd, int stdout_fd) {
+    dup2(stdin_fd, STDIN_FILENO);
+    dup2(stdout_fd, STDOUT_FILENO);
+    close(stdin_fd);
+    close(stdout_fd);
 }
 
 int main() {
     char input[256];
     char output[256];
     size_t bytes;
+    char *path = getenv("PATH");
+    char *new_path = new_string(strlen(custom_binary) + strlen(path) + 1);
+    strcat(new_path, custom_binary);
+    strcat(new_path, path);
+    setenv("PATH", new_path, 1);
 
     // struct sigaction sa;
-
+    
     // sa.sa_handler = SIG_IGN;
     // sa.sa_flags = 0;
     // sigemptyset(&sa.sa_mask);
@@ -60,13 +88,78 @@ int main() {
             write(STDERR_FILENO, "swsh: Command not found\n", 24);
             continue;
         }
-        
+
+        int pipe_fds[2];
+        int prev_pipe_fd = -1;
+
         for (int i = 0; i < cmd_cnt; ++i) {
             print_command(cmds[i]);
+            COMMAND *cmd = cmds[i];
+        
+            if (i < cmd_cnt - 1) {
+                if (pipe(pipe_fds) == -1) {
+                    perror("pipe");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            int stdin_fd = dup(STDIN_FILENO);
+            int stdout_fd = dup(STDOUT_FILENO);
+
+            if (cmd->need_fork) {
+                pid_t pid = fork();
+                if (pid == 0) {
+                    setup_redirection(cmd, i == 0, i == cmd_cnt - 1, prev_pipe_fd, pipe_fds);
+
+                    if (i < cmd_cnt - 1) {
+                        close(pipe_fds[0]);
+                        close(pipe_fds[1]);
+                    }
+
+                    if (prev_pipe_fd != -1) {
+                        close(prev_pipe_fd);
+                    }
+
+                    execvp(cmd->cmd, cmd->argv);
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                // setup_redirection(cmd, i == 0, i == cmd_cnt - 1, prev_pipe_fd, pipe_fds);
+                if (strcmp(cmd->cmd, "cd") == 0) {
+                    cd(cmd->argv[1]);
+                } else if (strcmp(cmd->cmd, "pwd") == 0) {
+                    char *wd = pwd();
+                    write(STDOUT_FILENO, wd, strlen(wd));
+                    free(wd);
+                } else {
+                    for (int j = 0; j < cmd_cnt; ++j) {
+                        COMMAND_free(cmds[j]);
+                    }
+                    free(cmds);
+                    unlink(INPUT_PIPE);
+                    unlink(OUTPUT_PIPE);
+                    shell_exit(cmd->argc > 1 ? atoi_32(cmd->argv[1]) : 0);
+                }
+                // restore_redirection(stdin_fd, stdout_fd);
+            }
+
+            int status;
+            waitpid(-1, &status, 0);
+            
+            if (prev_pipe_fd != -1) {
+                close(prev_pipe_fd);
+            }
+            if (i < cmd_cnt - 1) {
+                close(pipe_fds[1]);
+                prev_pipe_fd = pipe_fds[0];
+            }
+
+            restore_redirection(stdin_fd, stdout_fd);
         }
+
         for (int i = 0; i < cmd_cnt; ++i) {
             COMMAND_free(cmds[i]);
         }
-        // write(1, input, bytes);
+        free(cmds);
     }
 }
